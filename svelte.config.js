335dea92321e4cc13fc3d930ab9cd616685ed702
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-04-23 17:36:07 +0200

    Add adapter for GH pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from "@sveltejs/adapter-auto"
+import adapterGhpages from "svelte-adapter-ghpages";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -18,12 +19,14 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    // Use different adapters based on the environment. 
+    // For GitHub Pages deployment, use svelte-adapter-ghpages. 
+    // For local development, you can use a different adapter like @sveltejs/adapter-static or any other suitable one.
+    adapter: process.env.NODE_ENV === 'production' ? adapterGhpages() : adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? "/BreakOften" : "",
+    },
   },
 }
 
