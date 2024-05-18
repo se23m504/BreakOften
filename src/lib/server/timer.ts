0c65254f4a0a45aa0115d34371783d3d0ac48eb8
Santiago Lo Coco <se23m504@technikum-wien.at>
2024-05-18 16:32:32 +0200

    Reformat

--- src/lib/server/timer.ts
+++ src/lib/server/timer.ts
@@ -1,13 +1,19 @@
-
-export const updateTimer = async ({ user, breakInterval, miniBreakDuration, longBreakDuration, soundEnabled, notificationsEnabled }) => {
-    await db.timer.update({
-      where: { user_id: user.id },
-      data: {
-        breakInterval,
-        miniBreakDuration,
-        longBreakDuration,
-        soundEnabled,
-        notificationsEnabled,
-      },
-    })
+export const updateTimer = async ({
+  user,
+  breakInterval,
+  miniBreakDuration,
+  longBreakDuration,
+  soundEnabled,
+  notificationsEnabled,
+}) => {
+  await db.timer.update({
+    where: { user_id: user.id },
+    data: {
+      breakInterval,
+      miniBreakDuration,
+      longBreakDuration,
+      soundEnabled,
+      notificationsEnabled,
+    },
+  })
 }
